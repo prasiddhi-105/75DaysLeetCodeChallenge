88e57babd6e53e76b0e4a28302c8c03f6d2bc61c
Prasiddhi Mishra <bhumimishra91@gmail.com>
2026-05-30 20:14:54 +0530

    Time: 54 ms (32.97%), Space: 13.8 MB (56.73%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,42 @@
+class Solution:
+    def pacificAtlantic(self, heights):
+        if not heights:
+            return []
+
+        rows, cols = len(heights), len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visited):
+            visited.add((r, c))
+
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if (0 <= nr < rows and
+                    0 <= nc < cols and
+                    (nr, nc) not in visited and
+                    heights[nr][nc] >= heights[r][c]):
+                    dfs(nr, nc, visited)
+
+        # Pacific Ocean (top row + left column)
+        for c in range(cols):
+            dfs(0, c, pacific)
+            dfs(rows - 1, c, atlantic)
+
+        # Atlantic Ocean (bottom row + right column)
+        for r in range(rows):
+            dfs(r, 0, pacific)
+            dfs(r, cols - 1, atlantic)
+
+        result = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    result.append([r, c])
+
+        return result
